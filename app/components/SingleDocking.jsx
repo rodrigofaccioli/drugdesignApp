4466cba18e2a7cf3ab3722ce3058dd7010c5d0d1
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2017-01-02 11:46:15 -0200

    Initial commit

---
+++ app/components/SingleDocking.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var SingleDocking = React.createClass({
+  render: function () {
+    return (
+      <h3>SingleDocking Component</h3>
+    )
+  }
+});
+
+module.exports = SingleDocking;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      About: 'app/components/About.jsx',
+      Examples: 'app/components/Examples.jsx',
+      SingleDocking: 'app/components/SingleDocking.jsx'
+      
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+};
